038575be75f531863a5a930bdef3949285b40f34
safaabouzaid <abouzaidsafa@gmail.com>
2024-03-04 21:11:42 +0300

    auth

--- Agency/Agency/settings.py
+++ Agency/Agency/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
-
+from datetime import timedelta
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -32,14 +32,15 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [#safaaa
     #sedra
-    'user' ,
+    
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'rest_framework' ,
+    #'rest_framework' ,
+    'rest_framework_simplejwt',
 ]
 
 MIDDLEWARE = [
@@ -81,6 +82,20 @@ DATABASES = {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
+}
+
+REST_FRAMEWORK = {
+
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    )
+}
+SIMPLE_JWT={
+    'ACCESS_TOKEN_LIFETIME':timedelta(days=15),
+    'REFRESH_TOKEN_LIFETIME':timedelta(days=1),
+    'BLACKLIST_AFTER_ROTATION':True,
+    'AUTH_HEADER_TYPES':('Bearer',),
+    'AUTH_TOKEN_CLASSES':('rest_framework_simplejwt.tockens.AccessToken',),
 }
 
 
--- Agency/user/models.py
+++ Agency/user/models.py
@@ -10,9 +10,7 @@ from django.db import models
 
 
 
-class Customer(model.Models):
-    user_id=models.IntegerField(max_length=10),
-    phone=models.CharField(max_length=20),
-    address=models.CharField(max_length=30),
-    gender=models.CharField(max_length=20),
-    passport_number=models.IntegerField(max_length=30),
+
+
+
+    
