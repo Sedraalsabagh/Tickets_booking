09ddaf2d49806f2cbf6eb016d8efd14c83ea7b2a
Sedraalsabagh <sedraalsabbagh2021@gmail.com>
2024-03-04 16:24:02 +0300

    sedra

--- Agency/Agency/settings.py
+++ Agency/Agency/settings.py
@@ -32,12 +32,14 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [#safaaa
     #sedra
+    'user' ,
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework' ,
 ]
 
 MIDDLEWARE = [
