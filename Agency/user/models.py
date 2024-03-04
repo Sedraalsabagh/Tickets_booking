75256bc80679d890f1bc5a1832b8dedfef721e94
safaabouzaid <abouzaidsafa@gmail.com>
2024-03-04 20:37:33 +0300

    auth

--- Agency/user/models.py
+++ Agency/user/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+
+
+
+
+
+
+
+
+
+class Customer(model.Models):
+    user_id=models.IntegerField(max_length=10),
+    phone=models.CharField(max_length=20),
+    address=models.CharField(max_length=30),
+    gender=models.CharField(max_length=20),
+    passport_number=models.IntegerField(max_length=30),
