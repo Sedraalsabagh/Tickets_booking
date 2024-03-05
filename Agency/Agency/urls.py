70823b6447df7c2e9a8689eebe07b3a5ed9e30f3
safaabouzaid <abouzaidsafa@gmail.com>
2024-03-05 20:58:52 +0300

    edit

--- Agency/Agency/urls.py
+++ Agency/Agency/urls.py
@@ -19,8 +19,7 @@ from django.urls import path,include
 from rest_framework_simplejwt.views import TokenObtainPairView
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/',include("account.urls")),
+    path('api/',include('account.urls')),
     path('api/token/',TokenObtainPairView.as_view()),
-
+    path('admin/', admin.site.urls),
 ]
