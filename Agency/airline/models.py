5049bb6f8d852a8ea1e2e875550f85c0ae578c6b
Sedraalsabagh <sedraalsabbagh2021@gmail.com>
2024-03-06 21:49:12 +0300

    ..

--- Agency/airline/models.py
+++ Agency/airline/models.py
@@ -2,12 +2,11 @@ from django.db import models
 
 # Create your models here.
 
-from django.db import models
 from datetime import timedelta
 
   
 class Policy(models.Model) :
-    policy_id=models.IntegerField(max_length=10,blank=False)
+    policy_id=models.IntegerField(blank=False)
     refundable=models.BooleanField(default=False,blank=False)
     exchangeable=models.BooleanField(default=False,blank=False)
     exchangeable_condation=models.TextField(default=False,blank=False)
@@ -17,7 +16,7 @@ class Policy(models.Model) :
         
     
 class Airline (models.Model) :
-   airline_id=models.IntegerField(max_length=10,blank=False)
+   airline_id=models.IntegerField(blank=False)
    airline_name=models.CharField(max_length=20) 
    description=models.TimeField(max_length=400)
    policy_id=models.ForeignKey(Policy,on_delete=models.CASCADE)  
--- Agency/flight/models.py
+++ Agency/flight/models.py
@@ -3,10 +3,9 @@ from datetime import timedelta,datetime
 
 from airline.models import Airline
 
-# Create your models here.
 
 class Flight(models.Model):
-    flight_id=models.IntegerField(max_length=10,blank=False)
+    flight_id=models.IntegerField(blank=False)
     id_airline=models.ForeignKey(Airline,on_delete=models.CASCADE)
     dateTime=models.DateTimeField(default=datetime.now)
     duration =models.DurationField(default=timedelta(days=0))
@@ -23,7 +22,7 @@ class FlightSeatClass(models.Model):
     seats_type=models.IntegerField()
     flight_id=models.ForeignKey(Flight,on_delete=models.CASCADE)
     price=models.FloatField(max_length=100)
-    count=models.IntegerField(max_length=100)
+    count=models.IntegerField()
     def __str__(self):
       return self.seats_type
 
