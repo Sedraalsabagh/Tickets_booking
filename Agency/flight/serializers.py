28f49fb4232bc1fb403ca48c4a7511d4422ac6e1
safaabouzaid <abouzaidsafa@gmail.com>
2024-03-06 21:25:50 +0300

    reviews

---
+++ Agency/flight/serializers.py
@@ -0,0 +1,16 @@
+from rest_framework import serializers
+from .models import Flight
+
+
+class FlightSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model=Flight
+        fields="__all__"
+
+class ReviewSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model=Review
+        fields="__all__"
+
